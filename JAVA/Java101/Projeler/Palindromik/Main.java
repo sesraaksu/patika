ae85158f40b39e7293facd5598db5b65753c4ca2
sesraaksu <sesraaksu@outlook.com>
2023-07-24 11:05:49 +0300

    changed

---
+++ JAVA/Java101/Projeler/Palindromik/Main.java
@@ -0,0 +1,26 @@
+package JAVA.Java101.Projeler.Palindromik;
+import java.util.Scanner;
+
+public class Main {
+    public static boolean isPalindrome(String word) {
+        String reverse = "";
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reverse += (word.charAt(i));
+        }
+
+        if (word.equals(reverse)) {
+            System.out.println("The input is palindrome");
+            return true;
+        }
+        else {
+            System.out.println("The input is not palindrome");
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the String : ");
+        String word = input.nextLine();
+        System.out.println(isPalindrome(word));
+    }
+}
