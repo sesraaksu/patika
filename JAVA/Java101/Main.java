27ccb1824afcd5e4b5c8d731f4378b38d91f7760
sesraaksu <sesraaksu@outlook.com>
2023-07-26 13:44:09 +0300

    changed

---
+++ JAVA/Java101/Main.java
@@ -0,0 +1,21 @@
+package JAVA.Java101;
+
+public class Main {
+
+    public static String letterCapitalize(String str){
+        String result = "";
+        String[] toArray = str.split(" ");
+
+        for(int i = 0; i < toArray.length; i++){
+            char[] wordString = toArray[i].toCharArray();
+            wordString[0] = Character.toUpperCase(wordString[0]);
+            result += new String(wordString) + " ";
+        }
+        return result.trim();
+    }
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println(letterCapitalize("i ran there"));
+    }
+
+}
